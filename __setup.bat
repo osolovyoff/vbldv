e18dd715427a2121e46c11afb52f76c3d5d63e7a
Oleg Solovyoff <osolovyoff@gmail.com>
2026-02-08 22:24:44 -0300

    5

--- __setup.bat
+++ __setup.bat
@@ -3,3 +3,15 @@ set "DIR=%~dp0"
 if "%DIR:~-1%"=="\" set "DIR=%DIR:~0,-1%"
 for /f "tokens=2*" %%A in ('reg query "HKCU\Environment" /v Path 2^>nul') do set "USER_PATH=%%B"
 if defined USER_PATH (setx PATH "%USER_PATH%;%DIR%") else (setx PATH "%DIR%")
+
+setx CHROME_PATH "c:\Program Files\Google\Chrome\Application\chrome.exe"
+
+rem v
+setx CHROME_PROFILE_2 "Default"
+
+rem m
+setx CHROME_PROFILE_1 "Profile 6"
+
+rem e
+setx CHROME_PROFILE_3 "Default"
+echo Environment variables set successfully.
