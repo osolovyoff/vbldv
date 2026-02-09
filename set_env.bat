e0753151176c70ba47ae0677f1ecf1441846f3f0
Oleg Solovyoff <osolovyoff@gmail.com>
2026-02-08 22:19:20 -0300

    Update set_env.bat

--- set_env.bat
+++ set_env.bat
@@ -1,6 +1,11 @@
 @echo off
 setx CHROME_PATH "c:\Program Files\Google\Chrome\Application\chrome.exe"
-setx CHROME_PROFILE_1 "Default"
-setx CHROME_PROFILE_2 "Profile 6"
+rem v
+setx CHROME_PROFILE_2 "Default"
+
+rem m
+setx CHROME_PROFILE_1 "Profile 1"
+
+rem e
 setx CHROME_PROFILE_3 "Profile 3"
 echo Environment variables set successfully.
